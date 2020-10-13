a29e992b37797f0c47341fd157506d72603f8c7a
peterwu <crowpeter27@gmail.com>
2020-10-13 20:42:42 +0800

    ver 1.3

--- stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#kill worker
-ps axf | grep worker_peter.py | grep -v grep | awk '{print "kill -15 " $1}' | sh
-ps axf | grep worker_example.py | grep -v grep | awk '{print "kill -15 " $1}' | sh
-#kill master
-ps axf | grep master_server_golden.py | grep -v grep | awk '{print "kill -15 " $1}' | sh
-ps axf | grep master_server_example.py | grep -v grep | awk '{print "kill -15 " $1}' | sh
